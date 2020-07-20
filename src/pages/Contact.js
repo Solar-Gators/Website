398843c3e89b45b8f9be1c81a5fe6c354f77c603
Raymond <loki_fosil@hotmail.com>
2020-07-19 20:21:57 -0400

    Remove extra console log

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -32,8 +32,6 @@ export default function Contact() {
         let name = document.getElementById('contact-name')?.value
         let msg = document.getElementById('contact-msg')?.value
 
-        console.log(document.getElementById('contact-email').value)
-
         if (!captcha) {
             return setErrorMsg("Please verify the reCaptcha.")
         }
