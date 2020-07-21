6585ae0ec8338876613153dc6d53e218c9d9a195
Raymond <loki_fosil@hotmail.com>
2020-07-20 22:46:22 -0400

    Add event page content

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
 
+import FullCalendar from '@fullcalendar/react'
+import googleCalendarPlugin from '@fullcalendar/google-calendar'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+
+
 export default function Events() {
     return (
-        <Container style={{margin: '25vh'}}>
-            <h2>
-                Event page coming soon...
-            </h2>
+        <Container className="ml-auto mr-auto" style={{margin: '5vh'}}>
+            <FullCalendar
+                plugins={[ googleCalendarPlugin, dayGridPlugin ]}
+                initialView="dayGridMonth"
+                googleCalendarApiKey='AIzaSyAHAyW347iPTLmSv8yL9WqitQ5YJnfHIXw'
+                events={{
+                    googleCalendarId: 'solargatorsweb@gmail.com'
+                }}
+            />
         </Container>
     )
 }
