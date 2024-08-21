c9700edde6be6deb56bcf5002793caafa63efa6c
mariapasaylo <mnapasaylo@gmail.com>
2024-08-20 22:39:50 -0400

    installed multi-carousel
    started  list for carousel

---
+++ src/content/assets/images/awards/AwardList.js
@@ -0,0 +1,16 @@
+import fsgp23 from "../content/assets/images/people/team22/victory0_crop_edit.jpg";
+export const AwardList = [
+    {
+        id: "1",
+        award_image: fsgp23,
+        photo_credits: "",
+        alt:"FGSP 2023"
+    },
+    {
+        id: "1",
+        award_image: fsgp23,
+        photo_credits: "",
+        alt:"FGSP 2023"
+    }
+
+]
