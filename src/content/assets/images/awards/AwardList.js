6cd7082c2434dfa50f6818ee3369a361e161dc07
mariapasaylo <mnapasaylo@gmail.com>
2024-08-22 16:15:13 -0400

    Added automatic carousel
    Still need to update the information for new awards

--- src/content/assets/images/awards/AwardList.js
+++ src/content/assets/images/awards/AwardList.js
@@ -1,16 +1,28 @@
-import fsgp23 from "../content/assets/images/people/team22/victory0_crop_edit.jpg";
+import fsgp23 from "../awards/victory0_crop_edit.jpg"
 export const AwardList = [
     {
         id: "1",
         award_image: fsgp23,
         photo_credits: "",
-        alt:"FGSP 2023"
+        photo_credits_link:"",
+        alt:"FGSP 2023",
+        description: "Our team achieved an electrifying <span class='accent_word' id='orange_accent'>victory at the 2023 Formula Sun Grand Prix (FSGP)</span>, solidifying our position as pioneers in solar-powered endurance racing. Battling against formidable competitors from around the country, our team demonstrated their exceptional engineering prowess and unyielding determination, ultimately crossing the finish line in an awe-inspiring <span class='accent_word' id='orange_accent'>1st place</span>. Additionally, our team received awards for <span class='accent_word' id='orange_accent'>Most Improved</span> and <span class='accent_word' id='orange_accent'>Aesthetics</span>.",
     },
     {
-        id: "1",
+        id: "2",
         award_image: fsgp23,
-        photo_credits: "",
-        alt:"FGSP 2023"
-    }
+        photo_credits: "Photo Credits to",
+        photo_credits_link:"",
+        alt:"Altair 2024",
+        description: "Altair",
+    },
+    {
+        id: "3",
+        award_image: fsgp23,
+        photo_credits: "Photo Credits to",
+        photo_credits_link:"",
+        alt:"FGSP 2024",
+        description: "FGSP 2024",
+    },
 
 ]
